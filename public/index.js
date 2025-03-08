2ab4718bfdd4fcd0130717f1b1f7dcbc8c30d7e8
Khanh Le <khanh.le@xolv.org>
2025-03-07 20:34:46 -0600

    Remove unused code

--- public/index.js
+++
@@ -1,8 +0,0 @@
-const getCurrentUser = async () => {
-    const response = await fetch('current-user');
-    return response.json();
-}
-getCurrentUser()
-.then(currentUser => {
-    document.getElementById('username').innerText = `Welcome ${currentUser.username}`;
-});
