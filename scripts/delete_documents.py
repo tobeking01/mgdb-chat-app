4ca89a96a79abf76e471088f3a862de90e2edbbf
zadkiel2001 <lekhuenhatanh@gmail.com>
2025-03-26 20:15:30 -0500

    Update delete_documents.py

--- scripts/delete_documents.py
+++ scripts/delete_documents.py
@@ -1,14 +1,13 @@
+from dotenv import load_dotenv
+import os
 from pymongo import MongoClient
-
-# Connect to your MongoDB Atlas cluster
-client = MongoClient("mongodb+srv://zadkiel:GAuHRbVvbVXW3TUQ@cluster0.hy50q.mongodb.net/?retryWrites=true&w=majority")
+load_dotenv()
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
 db = client["chatDb"]
-
-# Delete all documents from the collections
 db.messages.delete_many({})
 db.organizations.delete_many({})
 db.roles.delete_many({})
 db.rooms.delete_many({})
 db.users.delete_many({})
-
-print("All documents have been deleted from messages, organizations, roles, rooms, and users collections.")
+print("All documents deleted from messages, organizations, roles, rooms, and users collections.")
